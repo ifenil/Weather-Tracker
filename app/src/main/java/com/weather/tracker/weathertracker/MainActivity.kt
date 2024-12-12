577850458171d0755ee7276017709741b9fc940b
Fenil <fenil.dobariya@outlook.com>
2024-12-12 12:53:53 -0500

    MainActivity and permission added in manifest

---
+++ app/src/main/java/com/weather/tracker/weathertracker/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.weather.tracker.weathertracker
+
+import android.app.Application
+import android.content.Context
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+       setContent {
+
+        }
+    }
+}
+
+@HiltAndroidApp
+class WeatherApplication : Application()
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideContext(application: Application): Context {
+        return application.applicationContext
+    }
+}
