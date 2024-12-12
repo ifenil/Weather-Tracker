a6ce0c64a3ef131b80689263b576fb8a1e674728
Fenil <fenil.dobariya@outlook.com>
2024-12-12 12:58:33 -0500

    SharedPref.kt added

---
+++ app/src/main/java/com/weather/tracker/weathertracker/SharedPref.kt
@@ -0,0 +1,25 @@
+package com.weather.tracker.nooro
+
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class SharedPreferencesManager @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences("WeatherAppPreferences", Context.MODE_PRIVATE)
+
+    private val cityKey = "saved_city"
+
+    // Save the city name
+    fun saveCity(city: String) {
+        sharedPreferences.edit().putString(cityKey, city).apply()
+    }
+
+    // Get the saved city name
+    fun getSavedCity(): String? {
+        return sharedPreferences.getString(cityKey, null)
+    }
+}
